3f38325ecc5022361815746ea2d56a82fe98d12d
Carl Hu <wintergrass@126.com>
2015-07-08 17:04:37 +0800

    111

---
+++ T/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  T
+//
+//  Created by iosdev on 15/7/8.
+//  Copyright (c) 2015年 Carl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    NSLog(@"1212");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ T/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  T
+//
+//  Created by iosdev on 15/7/8.
+//  Copyright (c) 2015年 Carl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
